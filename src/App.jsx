92cbefa4143713e54d1e6535bd482f31a3d4b932
Kazi <kazi98@proton.me>
2023-06-12 16:19:46 -0500

    imported puppyList data to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import {puppyList} from './data.js'
+import {useState} from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+  
   return (
-    <>
+    
       <div>
-       
+        {
+          puppyList.map((puppy) => {
+
+          return <p key={puppy.id}>{puppy.name}</p>;
+
+        })
+        }
       </div>
       
-    </>
+  
   )
 }
 
